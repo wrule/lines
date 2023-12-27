f88da4ec976a32365cf77174e18b2e314f65f1de
jimao <jimao@jimaodeMacBook-Pro.local>
2023-12-27 12:13:07 +0800

    lines

---
+++ src/views/home/Lines/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from './index.module.scss';
+
+export
+function Lines() {
+  return <div className={style.com}>
+    lines
+  </div>;
+}
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,11 +1,13 @@
 import { Button, Row } from 'antd';
 import style from './index.module.scss';
+import { Lines } from './Lines';
 
 export
 function Home() {
   return <div className={style.com}>
-    <Row>
+    <Lines />
+    {/* <Row>
       <Button type="primary">你好，世界</Button>
-    </Row>
+    </Row> */}
   </div>;
 }
