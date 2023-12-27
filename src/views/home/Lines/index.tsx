2e4e868e97247e6731cbb645a7c5e495cc1a6e88
jimao <jimao@jimaodeMacBook-Pro.local>
2023-12-27 12:26:18 +0800

    mockData

--- src/views/home/Lines/index.tsx
+++ src/views/home/Lines/index.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useRef } from 'react';
+import dayjs from 'dayjs';
 import { createChart } from 'lightweight-charts';
 import style from './index.module.scss';
+
+const mockData = Array(1000).fill(0).map((_, index) => ({
+  time: dayjs('2023-01-01').add(index, 'days').format('YYYY-MM-DD'),
+  value: Math.random() * 100,
+}));
 
 export
 function Lines(props: {
@@ -15,18 +21,7 @@ function Lines(props: {
       height: props.height ?? 220,
     });
     const lineSeries = chart.addLineSeries();
-    lineSeries.setData([
-        { time: '2019-04-11', value: 80.01 },
-        { time: '2019-04-12', value: 96.63 },
-        { time: '2019-04-13', value: 76.64 },
-        { time: '2019-04-14', value: 81.89 },
-        { time: '2019-04-15', value: 74.43 },
-        { time: '2019-04-16', value: 80.01 },
-        { time: '2019-04-17', value: 96.63 },
-        { time: '2019-04-18', value: 76.64 },
-        { time: '2019-04-19', value: 81.89 },
-        { time: '2019-04-20', value: 74.43 },
-    ]);
+    lineSeries.setData(mockData);
   }, []);
 
   return <div ref={self} className={style.com}></div>;
