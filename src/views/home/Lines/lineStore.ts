bcac2aacdad3b1fd2066c2bbdb6f09c7ac666ff6
jimao <jimao@jimaodeMacBook-Pro.local>
2023-12-29 14:48:59 +0800

    lineStore

--- src/views/home/Lines/lineStore.ts
+++ src/views/home/Lines/lineStore.ts
@@ -8,5 +8,35 @@ interface Line {
 
 export
 class LineStore {
+  public legendMouseEnter(type: string) {
 
+  }
+
+  public legendMouseLeave(type: string) {
+
+  }
+
+  public legendClick(type: string) {
+
+  }
+
+  public lineMouseEnter(type: string) {
+
+  }
+
+  public lineMouseLeave(type: string) {
+
+  }
+
+  public filter(userInput: string) {
+
+  }
+
+  public filterApply(userInput: string) {
+
+  }
+
+  public reset() {
+
+  }
 }
