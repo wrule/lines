89f77af90b976edfae38a3b7d47d648e18264f50
jimao <jimao@jimaodeMacBook-Pro.local>
2023-12-29 13:18:15 +0800

    提交代码

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,9 +1,11 @@
 import { Button, Row } from 'antd';
 import style from './index.module.scss';
 import { Lines } from './Lines';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-function newPoints(num = 5) {
+const LINE_MAX = 5;
+
+function newPoints(num: number) {
   const time = Date.now();
   return Array(num).fill(0).map((_, index) => ({
     type: `类型${index + 1}`,
@@ -13,11 +15,20 @@ function newPoints(num = 5) {
 
 export
 function Home() {
+  const [data, setData] = useState<any[]>([]);
+
   useEffect(() => {
-    console.log(newPoints());
-  }, []);
+    const timer = setTimeout(() => {
+      const appendData = newPoints(LINE_MAX);
+      const newData = [...data, ...appendData];
+      console.log(newData.length, appendData);
+      setData(newData);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [data]);
 
   return <div className={style.com}>
+    {data.length}
     {/* <Lines /> */}
     {/* <Row>
       <Button type="primary">你好，世界</Button>
