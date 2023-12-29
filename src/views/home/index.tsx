b1d577c1ca85be30ba2bea4844300dbdfa5e5820
jimao <jimao@jimaodeMacBook-Pro.local>
2023-12-29 13:04:49 +0800

    提交代码

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,19 +1,24 @@
 import { Button, Row } from 'antd';
 import style from './index.module.scss';
 import { Lines } from './Lines';
+import { useEffect } from 'react';
 
 const LINE_MAX = 5;
 
-function newPoints(num: number) {
+function newPoints() {
   const time = Date.now();
   return Array(LINE_MAX).fill(0).map((_, index) => `类型${index + 1}`)
-    .map((type) => Array(num).fill(0).map(() => ({
+    .map((type) => Array(1).fill(0).map(() => ({
       type, time, value: Math.random() * 100,
     }))).flat();
 }
 
 export
 function Home() {
+  useEffect(() => {
+    console.log(newPoints());
+  }, []);
+
   return <div className={style.com}>
     {/* <Lines /> */}
     {/* <Row>
